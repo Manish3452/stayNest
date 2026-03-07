17e8a2e3b0d77c25a4f0026c81a8facc8489afe1
MANISH KASAUDHAN <153519904+Manish3452@users.noreply.github.com>
2026-03-08 01:02:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -91,12 +91,11 @@ app.use("/host", hostRouter);
 
 app.use(errorsController.pageNotFound);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(DB_PATH).then(() => {  
   console.log('✅ Mongoose connected to MongoDB');
-app.listen(PORT, () => {
-    console.log(`Server running on address http://localhost:${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
-}).catch(err => {
-  console.log('❌ Mongoose connection failed', err);
 });
